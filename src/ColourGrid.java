ccb01b1ea655e2c93412c269d4e6973a474aa9be
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-07-24 10:24:59 +0530

    Create selectLargestConnectingBlock() Method

--- src/ColourGrid.java
+++ src/ColourGrid.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 
+
 public class ColourGrid {
     public static void main(String[] args) {
         int rows = 15;
@@ -7,27 +8,42 @@ public class ColourGrid {
         int numOfColours = 4;
 
         Grid[][] grids = createGrid(rows, columns, numOfColours);
-        selectLargestConnectingBlock(grids,rows,columns);
+        selectLargestConnectingBlock(grids, rows, columns);
 
     }
 
     private static void selectLargestConnectingBlock(Grid[][] grids, int rows, int columns) {
-        int[][] counts = new int[rows][columns];
         int maxX = 0;
         int maxY = 0;
         int maxCount = 0;
+        int[][] counts = new int[rows][columns];
 
-        for (int i = 0; i < grids.length; i++) {
-            for (int j = 0; j < rows; j++) {
-                for (int k = 0; k < columns; k++) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
 
+                if (checkInside(i, j + 1) && grids[i][j].getColour() == grids[i][j + 1].getColour()) {
+                    counts[i][j] += counts[i + 1][j] + 1;
                 }
+
+                if (checkInside(i, j - 1) && grids[i][j].getColour() == grids[i][j - 1].getColour()) {
+                    counts[i][j] += counts[i + 1][j] + 1;
+                }
+
+                if (checkInside(i + 1, j) && grids[i][j].getColour() == grids[i + 1][j].getColour()) {
+                    counts[i][j] += counts[i + 1][j] + 1;
+                }
+
+                if (checkInside(i - 1, j) && grids[i][j].getColour() == grids[i - 1][j].getColour()) {
+                    counts[i][j] += counts[i - 1][j] + 1;
+                }
+
             }
         }
+
     }
 
-    private static boolean isInside(int i, int j,int rows, int columns) {
-        return (-1 < i && -1 < j && i < rows && j < columns);
+    private static boolean checkInside(int i, int j) {
+        return false;
     }
 
     private static Grid[][] createGrid(int rows, int columns, int numOfColours) {
